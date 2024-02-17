75bcfc9a4bcb62a1c7d5451d744380ebeb935032
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2024-02-17 10:12:33 -0300

    fix direction axios film

--- films/src/controllers/getFilm.js
+++ films/src/controllers/getFilm.js
@@ -2,6 +2,7 @@ const films = require("../data")
 
 module.exports = async (req, res) => {
     var perro = await films.list()
+    console.log(perro)
     //throw Error("se fue a la mierda")
     res.status(200).json(perro)
 }
--- films/src/data/index.js
+++ films/src/data/index.js
@@ -4,11 +4,11 @@ const axios = require("axios")
 module.exports = {
     list:  async () => {
             //var valor = await axios.get("http://database:8002/Film")
-            var valor = await axios.get("http://localhost:8002/Film")
+            var valor = await axios.get("http://localhost:8004/Film")
             return valor.data
         },
     listOne: async (hola) => {
-            var valor = await axios.get(`http://database:8002/Film/${hola}`)
+            var valor = await axios.get(`http://localhost:8004/Film/${hola}`)
             return valor.data
         },
 }

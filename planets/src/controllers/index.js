2e995d6c4d06877bdd1ea3ca8b57d172b19ca835
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2024-02-21 22:14:00 -0300

    add dates for Planet Microservices

--- planets/src/controllers/index.js
+++ planets/src/controllers/index.js
@@ -1,4 +1,6 @@
 
+
 module.exports = {
+    
     getPlanets: require("./getPlanets")
 }
--- planets/src/data/index.js
+++ planets/src/data/index.js
@@ -1,9 +1,9 @@
-//const valor = require("./planets.json")
-const axios = require("axios")
+const planet = require("./planets.json")
+//const axios = require("axios")
 
 module.exports = {
     list: async () => {
-        var valor = await axios.get("http://database:8003/Planet")
-        return valor.data
+        //var valor = await axios.get("http://localhost:8004/Planet")
+        return planet
     },
 }

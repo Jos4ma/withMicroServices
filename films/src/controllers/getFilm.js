e30e78d3f4a257bbf7eb2b17afc5dffb9183ff72
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2024-02-24 20:48:43 -0300

    response film personal

--- films/src/controllers/getFilm.js
+++ films/src/controllers/getFilm.js
@@ -1,9 +1,11 @@
 const films = require("../data")
+const {response} = require("../utils/index")
 
 module.exports = async (req, res) => {
     var {_id} = req.params
-    if (!_id)   var film = await films.list()
+    if (!_id)   var movie = await films.list()
     //throw Error("se fue a la mierda")
-    else    var film = await films.listOne(_id)
-    res.status(200).json(film)
+    else    var movie = await films.listOne(_id)
+    //res.status(200).json(film)
+    response( res, 200, movie)
 }
